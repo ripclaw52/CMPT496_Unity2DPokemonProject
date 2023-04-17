5a2cf6ba6adf5e4ed2b5fdce631517e37c1076c1
Ryley Goodine <ryleygoodine96@gmail.com>
2023-04-17 11:14:33 -0600

    Completed Tutorial #48
    Persisting Scene States
    
    -Saving the states of a scene before unloading it
    -Fixing issue where previous scene is not unloaded

--- Assets/Scripts/SceneManagement/SceneDetails.cs
+++ Assets/Scripts/SceneManagement/SceneDetails.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,8 @@ public class SceneDetails : MonoBehaviour
     [SerializeField] List<SceneDetails> connectedScenes;
 
     public bool IsLoaded { get; private set; }
+
+    List<SavableEntity> savableEntities;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -25,14 +28,18 @@ public class SceneDetails : MonoBehaviour
             }
 
             // Unload the scenes that are no longer connected
-            if (GameController.Instance.PrevScene != null)
+            var prevScene = GameController.Instance.PrevScene;
+            if (prevScene != null)
             {
-                var previouslyLoadedScenes = GameController.Instance.PrevScene.connectedScenes;
+                var previouslyLoadedScenes = prevScene.connectedScenes;
                 foreach (var scene in previouslyLoadedScenes)
                 {
                     if (!connectedScenes.Contains(scene) && scene != this)
                         scene.UnloadScene();
                 }
+
+                if (!connectedScenes.Contains(prevScene))
+                prevScene.UnloadScene();
             }
         }
     }
@@ -41,8 +48,14 @@ public class SceneDetails : MonoBehaviour
     {
         if (!IsLoaded)
         {
-            SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
+            var operation = SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
             IsLoaded = true;
+
+            operation.completed += (AsyncOperation op) =>
+            {
+                savableEntities = GetSavableEntitiesInScene();
+                SavingSystem.i.RestoreEntityStates(savableEntities);
+            };
         }
     }
 
@@ -50,8 +63,17 @@ public class SceneDetails : MonoBehaviour
     {
         if (IsLoaded)
         {
+            SavingSystem.i.CaptureEntityStates(savableEntities);
+
             SceneManager.UnloadSceneAsync(gameObject.name);
             IsLoaded = false;
         }
+    }
+
+    List<SavableEntity> GetSavableEntitiesInScene()
+    {
+        var currScene = SceneManager.GetSceneByName(gameObject.name);
+        var savableEntities = FindObjectsOfType<SavableEntity>().Where(x => x.gameObject.scene == currScene).ToList();
+        return savableEntities;
     }
 }
