72e94d193e24d14eaa5b0ec1c903b5af553815b2
Ryley Goodine <ryleygoodine96@gmail.com>
2023-08-18 14:41:42 -0600

    Completed Tutorial #98
    Customizing Cutscene Actor Selection UI
    
    -Customize Actor Selection UI using Custom Property Drawers
    -Only show the Character Object Field if IsPlayer toggle is off

---
+++ Assets/Scripts/Cutscenes/Editor/CutsceneActorDrawer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(CutsceneActor))]
+public class CutsceneActorDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        position = EditorGUI.PrefixLabel(position, label);
+
+        var togglePos = new Rect(position.x, position.y, 70, position.height);
+        var fieldPos = new Rect(position.x + togglePos.width, position.y, position.width - togglePos.width, position.height);
+
+        var isPlayerProp = property.FindPropertyRelative("isPlayer");
+
+        isPlayerProp.boolValue = GUI.Toggle(togglePos, isPlayerProp.boolValue, "Is Player");
+        isPlayerProp.serializedObject.ApplyModifiedProperties();
+
+        if (!isPlayerProp.boolValue)
+            EditorGUI.PropertyField(fieldPos, property.FindPropertyRelative("character"), GUIContent.none);
+
+        EditorGUI.EndProperty();
+    }
+}
