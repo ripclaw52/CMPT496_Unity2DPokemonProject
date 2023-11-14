35e1347e565ceb12fd755eec8ea61841bc36a935
Ryley Goodine <ryleygoodine96@gmail.com>
2023-11-13 19:14:48 -0700

    Initial box class

---
+++ Assets/Scripts/PCBox/Box.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Box
+{
+    List<Pokemon?> boxList = Enumerable.Repeat((Pokemon?)null, 30).ToList();
+}
